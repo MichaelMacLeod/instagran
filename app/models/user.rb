4cdffab1a8b00e61ae188f4167a24f05ce95c2f9
MichaelMacLeod <edinburghmacleod@gmail.com>
2016-01-12 14:27:37 +0000

    Links in nav bar to Index and New Post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
   has_many :posts
+
+  has_many :friendships, :class_name => 'Friend', :foreign_key => 'self_id'
+  has_many :inverse_friendships, :class_name => 'Friend', :foreign_key => 'friend_id'
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
