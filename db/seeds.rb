cc9d09ed14a4e535c4592c55374c0022c164f515
MichaelMacLeod <edinburghmacleod@gmail.com>
2016-01-13 09:58:13 +0000

    Added hello name and log out on navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.delete_all
+Post.delete_all
+
+u1 = User.create(email: 'test@mike.com', password: 'password')
+p1 = Post.create(caption: 'Random one', user: u1)
+p1.post_image = Rails.root.join("db/seed_images/Cow.png").open
+p1.save
+
+u2 = User.create(email: 'gary@test.com', password: 'password')
+p2 = Post.create(caption: 'Panda', user: u2)
+p2.post_image = Rails.root.join("db/seed_images/Panda.png").open
+p2.save
